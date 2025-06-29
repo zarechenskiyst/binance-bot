8121ead7f5eb1437f6cfd96d278d54514e239e06
Stanislav <Zarechenskiyst@gmail.com>
2025-06-29 19:13:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def execute_trade(symbol, signal):
         ticker = client.get_symbol_ticker(symbol=symbol)
         price = float(ticker['price'])
         qty = get_trade_quantity(symbol, TRADE_AMOUNT, price)
-        qty_str="{0:.8}".format(qty).rstrip('0').rstrip('.')
+        qty_str=f"{0:.8}".format(qty).rstrip('0').rstrip('.')
 
         print(f"[DEBUG] qty: {qty_str} | type: {type(qty_str)}")
 
