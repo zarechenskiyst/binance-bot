c8100524c20897f38a00d775c4bc1b6c023ca92c
Stanislav <Zarechenskiyst@gmail.com>
2025-06-30 19:47:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ client.API_URL = 'https://testnet.binance.vision/api'
 
 # 🔄 Торгуемые пары
 raw_symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'ADAUSDT', 'MATICUSDT', 'DOTUSDT', 'LINKUSDT', 'AVAXUSDT', 'XRPUSDT', 'PEPEUSDT']
+
+# Получаем список всех доступных символов на Binance
+exchange_info = client.get_exchange_info()
+valid_binance_symbols = {s['symbol'] for s in exchange_info['symbols']}
 symbols = [s for s in raw_symbols if s in valid_binance_symbols]
 
 interval = Client.KLINE_INTERVAL_5MINUTE
