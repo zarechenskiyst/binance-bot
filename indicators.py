240d82db6da05c41749621d694e3e42d7231e51f
Stanislav <Zarechenskiyst@gmail.com>
2025-06-30 19:37:03 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+# 📌 RSI
+def compute_rsi(series, period=14):
+    delta = series.diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+    avg_gain = gain.rolling(window=period).mean()
+    avg_loss = loss.rolling(window=period).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
