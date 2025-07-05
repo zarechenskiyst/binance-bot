b8836c698db49c17c8533c198c6006abd58d0d4e
Stanislav <Zarechenskiyst@gmail.com>
2025-07-05 22:48:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ REPORT_HOUR = 20  # час (0–23) отправки ежедневного от
 def load_trade_history():
     if trade_log_all is []:
         with open(HISTORY_FILE, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+            data = json.load(f)
         # Приводим timestamp из строк в datetime
         for t in data:
             t['timestamp'] = datetime.fromisoformat(t['timestamp'])
